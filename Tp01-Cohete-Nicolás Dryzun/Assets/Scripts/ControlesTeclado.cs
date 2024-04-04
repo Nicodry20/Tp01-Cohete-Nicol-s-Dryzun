a038487a525994987f0b38dcd1ebc00d7c7ec815
Nicodry20 <48178309@est.ort.edu.ar>
2024-04-04 10:28:10 -0300

    aa

---
+++ Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/ControlesTeclado.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlesTeclado : MonoBehaviour
+{
+    public float movX;
+    public float speed;
+    public float maxX = 8;
+    public float minX = -8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movX = Input.GetAxis("Horizontal");
+        transform.Translate(movX * speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(maxX,transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < minX)
+        {
+
+        }
+    }
+}
