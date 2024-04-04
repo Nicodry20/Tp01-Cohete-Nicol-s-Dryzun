92c49dcdb9d223501de02365a58636e95eaf2239
Nicodry20 <48178309@est.ort.edu.ar>
2024-04-04 17:01:35 -0300

    Fuego hecho

--- Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/CoheteScript.cs
+++ Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/CoheteScript.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class CoheteScript : MonoBehaviour
 {
     public float speed;
+    public GameObject fuego; // Objeto de fuego
+
+    private bool coheteDespegado = false; // Variable para controlar si el cohete ha despegado
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fuego.SetActive(false); // Desactivar el fuego al inicio
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0,speed,0);
+        transform.Translate(0, speed, 0);
+
+        // Activar el fuego cuando el cohete despega
+        if (!coheteDespegado && speed > 0)
+        {
+            fuego.SetActive(true);
+            coheteDespegado = true;
+        }
     }
 }
--- Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/FuegoCohete.cs
+++ Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/FuegoCohete.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoheteFuego : MonoBehaviour
+public class FuegoCohete : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
