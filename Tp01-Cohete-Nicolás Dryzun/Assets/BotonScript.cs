125708b224ccdb87f11b0c21959c0a8044a61c69
Nicodry20 <48178309@est.ort.edu.ar>
2024-04-04 09:48:12 -0300

    Falta fuegito

---
+++ Tp01-Cohete-Nicolás Dryzun/Assets/BotonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonScript : MonoBehaviour
+{
+    public GameObject botonNormalGO;
+    public GameObject botonPresionadoGO;
+    public CoheteScript coheteScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        botonNormalGO.SetActive(true);
+        botonPresionadoGO.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Click");
+        botonNormalGO.SetActive(false);
+        botonPresionadoGO.SetActive(true);
+        coheteScript.speed = 0.1f;
+    }
+    private void OnMouseUp()
+    {
+        //Debug.Log("Realese");
+        botonNormalGO.SetActive(true);
+        botonPresionadoGO.SetActive(false);
+    }
+}
