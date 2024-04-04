79dddd86a6229c971982adceadc401dafbecb288
Nicodry20 <48178309@est.ort.edu.ar>
2024-04-04 10:51:07 -0300

    listo

--- Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/ControlesTeclado.cs
+++ Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/ControlesTeclado.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ControlesTeclado : MonoBehaviour
 {
     public float movX;
+    public float movY;
+    public float maxY = 3;
+    public float minY = -4;
     public float speed;
     public float maxX = 8;
     public float minX = -8;
@@ -18,7 +21,8 @@ public class ControlesTeclado : MonoBehaviour
     void Update()
     {
         movX = Input.GetAxis("Horizontal");
-        transform.Translate(movX * speed * Time.deltaTime, 0, 0);
+        movY = Input.GetAxis("Vertical");
+        transform.Translate(movX * speed * Time.deltaTime, movY * speed * Time.deltaTime, 0);
 
         if (transform.position.x > maxX)
         {
@@ -26,7 +30,18 @@ public class ControlesTeclado : MonoBehaviour
         }
         else if (transform.position.x < minX)
         {
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        }
 
+
+
+        if (transform.position.y > maxY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+        }
+        else if (transform.position.y < minY)
+        {
+            transform.position = new Vector3(transform.position.x, minY, transform.position.z);
         }
     }
 }
