9a845b78e7452175b4c4ae1508c82924d5d389fa
Nicodry20 <48178309@est.ort.edu.ar>
2024-04-25 10:40:37 -0300

    Tarea Arrays

---
+++ Tp01-Cohete-Nicolás Dryzun/Assets/Scripts/EjerciciosArrays.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjerciciosArrays : MonoBehaviour
+{
+    public float[] notas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Escribir el primer elemento del array
+        //Escribir la cantidad de elementos del array
+        //Escribir cada elemento del array
+        //Escribir el promedio de los elementos del array
+        //Escribir los elementos del array mayores que 9
+        //Escribir los elementos del array en orden inverso
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
